1fcff1c6d31aa486174bbc2febeb14b1492a40c1
Sam Kottler <sam@kottlerdevelopment.com>
2011-11-11 09:55:56 -0500

    Removed the upstart provider

--- puppet/modules/graylog2/manifests/web/service.pp
+++ puppet/modules/graylog2/manifests/web/service.pp
@@ -1,7 +1,6 @@
 class graylog2::web::service {
   service {'graylog2-web':
     ensure   => 'running',
-    provider => 'upstart',
     require  => [ Class['graylog2::server'], Class['graylog2::web::install'], Class['graylog2::web::config'], ],
   }
 }
