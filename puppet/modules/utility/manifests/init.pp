7ef41ae6fb2f28adc78deb7eae438ef205a28d64
Sam Kottler <sam@kottlerdevelopment.com>
2011-11-10 23:21:00 -0500

    Added the utility module for repos and random fixes. Wont be part of the ultimate repo

---
+++ puppet/modules/utility/manifests/init.pp
@@ -0,0 +1,36 @@
+class utility {
+
+  yumrepo { 'epel':
+    baseurl    => 'http://epel.osuosl.org/5/$basearch',
+    mirrorlist => 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-debug-5&arch=$basearch',
+    enabled    => 1,
+    gpgcheck   => 0,
+  }
+  
+  # Extra repo required for htop.
+  yumrepo { 'dag':
+    baseurl  => 'http://apt.sw.be/redhat/el$releasever/en/$basearch/dag',
+    gpgcheck => 0,
+    enabled  => 1,
+  }
+
+  package { 'htop':
+    ensure  => installed,
+    require => Yumrepo['dag'],
+  }
+
+  # Fix a bug with Vagrant by verbosely adding the puppet group and user.
+  group { "puppet":
+    ensure => present,
+    gid => 24,
+  }
+
+  user { "puppet":
+    ensure => present,
+    uid => 24,
+    gid => 24,
+    home => "/var/lib/puppet",
+    managehome => true,
+    require => Group["puppet"],
+  }
+}
