0b3119ab2aa5fc66b534d07c9d05e19bcc00cd35
Sam Kottler <sam@kottlerdevelopment.com>
2011-11-21 17:37:36 -0500

    Updated java and such

--- puppet/modules/utility/manifests/init.pp
+++ puppet/modules/utility/manifests/init.pp
@@ -6,17 +6,22 @@ class utility {
     enabled    => 1,
     gpgcheck   => 0,
   }
-  
+
   # Extra repo required for htop.
   yumrepo { 'dag':
     baseurl  => 'http://apt.sw.be/redhat/el$releasever/en/$basearch/dag',
     gpgcheck => 0,
     enabled  => 1,
   }
-  
+
   package { 'htop':
     ensure  => installed,
     require => Yumrepo['dag'],
+  }
+
+  package { 'java':
+    name => "java-1.6.0-openjdk.x86_64",
+    ensure => installed,
   }
 
   # Fix a bug with Vagrant by verbosely adding the puppet group and user.
